3142af6694127fe9a5d7c677640e3ef70091dd88
YanC Lin <yonalin1@gmail.com>
2018-03-20 22:17:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class App < Sinatra::Base
    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
  end
 
- get '/logout' do
+ get '/checkout' do
    session.clear
    "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
  end
