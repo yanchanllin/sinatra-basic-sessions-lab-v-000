48dbe40bc8d545bf4bf5b3d6b1941d8a54ad827c
YanC Lin <yonalin1@gmail.com>
2018-03-20 22:13:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   configure do
-    enable: sessions unless test?
+    enable :sessions unless test?
     set:session_secret, "secret"
   end
   before do
